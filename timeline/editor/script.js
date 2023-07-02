af6674f06a7f40dd44dafad70e0a86fd53440519
Ronak Kothari <rk.ronakjkothari@gmail.com>
2023-07-01 18:16:46 -0700

    Image Support
    Added ability to create events with images

--- timeline/editor/script.js
+++ timeline/editor/script.js
@@ -2,6 +2,127 @@ const nodes = [];
 let start;
 let end;
 
+const dragArea = document.getElementById("upload-image-container");
+
+dragArea.addEventListener("dragover", function(e){
+    document.getElementById("upload-file-image-header").innerHTML = "Release to Upload";
+    document.getElementById("upload-image-container").classList.add("active");
+});
+
+dragArea.addEventListener("dragleave", function(e){
+    document.getElementById("upload-file-image-header").innerHTML = "Drag & Drop or <span>Browse</span>";
+    document.getElementById("upload-image-container").classList.remove("active");
+});
+
+dragArea.addEventListener("drop", function(e){
+    e.preventDefault();
+
+    uploadImage(e.dataTransfer.files[0]);
+});
+
+document.getElementById("upload-file-image").addEventListener("change", function(e){
+    if (document.getElementById("upload-file-image").files.length === 0) {
+        alert("Please select a file");
+    } else {
+        uploadImage(document.getElementById("upload-file-image").files[0]);
+    }
+});
+
+function selectLibraryImage(image){
+    document.getElementById("uploaded-image").src = `../images/${image}.jpg`;
+    document.getElementById("uploaded-image-data").innerHTML = `../images/${image}.jpg`;
+
+    document.getElementById("event-banner-p").classList.add("inactive-modal");
+    document.getElementById("uploaded-image-container").classList.remove("inactive-modal");
+
+    closeUploadModal();
+}
+
+function uploadImage(file){
+    if(["image/jpeg", "image/jpg", "image/png", "image/gif"].includes(file.type)){
+        const fileReader = new FileReader();
+
+        fileReader.onload = function(){
+            let fileURL = fileReader.result;
+
+            document.getElementById("upload-file-image-header").innerHTML = "Drag & Drop or <span>Browse</span>";
+            document.getElementById("upload-image-container").classList.remove("active");
+
+            let image = new Image()
+            let canvas = document.createElement('canvas')
+            let ctx = canvas.getContext('2d');
+
+            image.src = fileURL;
+            canvas.width = 342;
+            canvas.height = 54;
+
+            image.onload = function(){
+                ctx = drawImageProp(ctx, image, 0, 0, 342, 54);
+
+                document.getElementById("uploaded-image").src = canvas.toDataURL("img/png", 1.0);
+                document.getElementById("uploaded-image-data").innerHTML = canvas.toDataURL("img/png", 1.0);
+                document.getElementById("event-banner-p").classList.add("inactive-modal");
+                document.getElementById("uploaded-image-container").classList.remove("inactive-modal");
+
+                closeUploadModal();
+            }
+        }
+
+        fileReader.readAsDataURL(file);
+    } else {
+        alert("Please upload an image file")
+    }
+}
+
+function drawImageProp(ctx, img, x, y, w, h, offsetX, offsetY) {
+
+    if (arguments.length === 2) {
+        x = y = 0;
+        w = ctx.canvas.width;
+        h = ctx.canvas.height;
+    }
+
+    // default offset is center
+    offsetX = typeof offsetX === "number" ? offsetX : 0.5;
+    offsetY = typeof offsetY === "number" ? offsetY : 0.5;
+
+    // keep bounds [0.0, 1.0]
+    if (offsetX < 0) offsetX = 0;
+    if (offsetY < 0) offsetY = 0;
+    if (offsetX > 1) offsetX = 1;
+    if (offsetY > 1) offsetY = 1;
+
+    var iw = img.width,
+        ih = img.height,
+        r = Math.min(w / iw, h / ih),
+        nw = iw * r,   // new prop. width
+        nh = ih * r,   // new prop. height
+        cx, cy, cw, ch, ar = 1;
+
+    // decide which gap to fill
+    if (nw < w) ar = w / nw;
+    if (Math.abs(ar - 1) < 1e-14 && nh < h) ar = h / nh;  // updated
+    nw *= ar;
+    nh *= ar;
+
+    // calc source rectangle
+    cw = iw / (nw / w);
+    ch = ih / (nh / h);
+
+    cx = (iw - cw) * offsetX;
+    cy = (ih - ch) * offsetY;
+
+    // make sure source rectangle is valid
+    if (cx < 0) cx = 0;
+    if (cy < 0) cy = 0;
+    if (cw > iw) cw = iw;
+    if (ch > ih) ch = ih;
+
+    // fill image in dest. rectangle
+    ctx.drawImage(img, cx, cy, cw, ch,  x, y, w, h);
+    return ctx;
+}
+
 function convertToText(date){
     const months = [
         "January",
@@ -36,6 +157,13 @@ function convertToText(date){
 
     return returnString;
 }
+
+document.getElementById("clear-uploaded-image").addEventListener("click", function(e){
+    document.getElementById("uploaded-image-container").classList.add("inactive-modal");
+    document.getElementById("uploaded-image").src = "";
+    document.getElementById("uploaded-image-data").textContent = "";
+    document.getElementById("event-banner-p").classList.remove("inactive-modal");
+})
 
 async function saveChanges(){
     const savedUserId = JSON.parse(localStorage.getItem('savedUserId'));
@@ -110,6 +238,12 @@ async function saveChanges(){
                 mode: 'cors'
             });
 
+            console.log({
+                timelineId:context.context.timeline.id,
+                name: nodes[i].name,
+                date: nodes[i].date,
+            });
+
             const context2 = response2.json();
             console.log(context2);
         }
@@ -118,6 +252,14 @@ async function saveChanges(){
 
 function leaveEditor(link){
     location.href = link;
+}
+
+function closeUploadModal(){
+    document.getElementById("upload-image-modal-container").classList.add("inactive-modal");
+}
+
+function openUploadModal(){
+    document.getElementById("upload-image-modal-container").classList.remove("inactive-modal");
 }
 
 function daysBetweenYears(year1, year2) {
@@ -166,10 +308,11 @@ function createTimelineEvent(params){
         nodes.splice(params.id, 1);
 
         refreshNodes();
-    })
+    });
 
     optionsDiv.append(editOption, deleteOption);
 
+    let headerImg = document.createElement("img");
     let date = document.createElement("h3");
     let title = document.createElement("h1");
     let desc = document.createElement("p");
@@ -183,7 +326,15 @@ function createTimelineEvent(params){
         date.innerHTML = `${convertToText(params.start)} - ${convertToText(params.end)}`;
     }
 
-    newDiv.append(optionsDiv, date, title, desc);
+    if(params.image !== ""){
+        headerImg.src = params.image;
+        newDiv.append(optionsDiv, headerImg, date, title, desc);
+
+        document.getElementById("uploaded-image").src = "";
+    } else {
+        newDiv.append(optionsDiv, date, title, desc);
+    }
+
     if(params.top){
         newDiv.classList.add("event-container-top");
     } else {
@@ -224,6 +375,11 @@ function createTimelineMarker(params){
 
     optionsDiv.append(editOption, deleteOption);
 
+    if(params.date === "Present Day"){
+        let date = new Date();
+        params.date = `${date.getFullYear()}-${date.getMonth() + 1}-${String(date.getDate()).padStart(2, '0')}`;
+    }
+
     date.innerHTML = convertToText(params.date);
     title.innerHTML = params.name;
 
@@ -234,6 +390,27 @@ function createTimelineMarker(params){
     console.log((daysBetweenYearStartAndDate(start,params.date)/(daysBetweenYears(start,end) - 1)) * document.getElementById('desktop-main-event-containers').clientWidth);
 
     document.getElementById("desktop-main-marker-container").appendChild(newDiv);
+}
+
+function toggleCheckbox(num){
+    if(document.querySelectorAll(".checkbox")[num].classList.contains("inactive-checkbox")){
+        document.querySelectorAll(".checkbox")[num].classList.remove("inactive-checkbox");
+
+        if(num === 0){
+            document.getElementById("new-marker-window").children[3].readOnly = true;
+            document.getElementById("new-marker-window").children[3].type = "input";
+            document.getElementById("new-marker-window").children[3].style.color = "gray";
+            document.getElementById("new-marker-window").children[3].value = "Present Day";
+        }
+    } else {
+        document.querySelectorAll(".checkbox")[num].classList.add("inactive-checkbox");
+
+        if(num === 0){
+            document.getElementById("new-marker-window").children[3].readOnly = false;
+            document.getElementById("new-marker-window").children[3].style.color = "black";
+            document.getElementById("new-marker-window").children[3].type = "date";
+        }
+    }
 }
 
 function refreshNodes(){
@@ -256,8 +433,13 @@ function refreshNodes(){
 
         nodes[0].top = true;
     } else if(nodes[0].type === "MARKER"){
-        start = parseInt(nodes[0].date.split("-")[0]);
-        end = parseInt(nodes[0].date.split("-")[0]);
+        if(nodes[0].date === "Present Day"){
+            start = new Date().getFullYear();
+            end = new Date().getFullYear();
+        } else {
+            start = parseInt(nodes[0].date.split("-")[0]);
+            end = parseInt(nodes[0].date.split("-")[0]);
+        }
     }
 
     nodes[0].id = 0;
@@ -276,10 +458,18 @@ function refreshNodes(){
                 }
             } nodes[i].top = eventCount % 2 === 0; eventCount++;
         } else if(nodes[i].type === "MARKER"){
-            if(parseInt(nodes[i].date.split("-")[0]) < start) {
-                start = parseInt(nodes[i].date.split("-")[0]);
-            } else if(parseInt(nodes[i].date.split("-")[0]) > end){
-                end = parseInt(nodes[i].date.split("-")[0]);
+            if(nodes[0].date === "Present Day"){
+                if(new Date().getFullYear() < start) {
+                    start = new Date().getFullYear();
+                } else if(new Date().getFullYear() > end){
+                    end = new Date().getFullYear();
+                }
+            } else {
+                if(parseInt(nodes[i].date.split("-")[0]) < start) {
+                    start = parseInt(nodes[i].date.split("-")[0]);
+                } else if(parseInt(nodes[i].date.split("-")[0]) > end){
+                    end = parseInt(nodes[i].date.split("-")[0]);
+                }
             }
         }
 
@@ -315,8 +505,9 @@ function createItem(){
             start: document.getElementById("new-event-window").children[3].value,
             end: document.getElementById("new-event-window").children[5].value,
             description: document.getElementById("new-event-window").children[7].value,
+            image: document.getElementById("uploaded-image-data").textContent,
             type: "EVENT"
-        })
+        });
     } else console.log("Something strange occurred");
 
     refreshNodes();
@@ -352,6 +543,11 @@ function closeCreateModal(){
         }
     }
 
+    document.getElementById("uploaded-image-container").classList.add("inactive-modal");
+    document.getElementById("uploaded-image").src = "";
+    document.getElementById("uploaded-image-data").textContent = "";
+    document.getElementById("event-banner-p").classList.remove("inactive-modal");
+
     eventNodes = document.getElementById("new-marker-window").children;
 
     for(let i = 0; i < eventNodes.length; i++){
@@ -377,6 +573,17 @@ async function initialize(user){
     document.getElementById('desktop-navbar-profile-name').innerHTML = user.firstName + " " + user.lastName;
     document.getElementById('desktop-navbar-profile-type').innerHTML = "Elephant " + user.type.charAt(0).toUpperCase() + user.type.substr(1).toLowerCase();
     document.getElementById('desktop-profile-user-img').src = "../../flip/icons/emojis/" + emojis_refactored[Math.floor(Math.random() * emojis_refactored.length)] + ".png";
+
+    for(let i = 0; i < 14; i++){
+        let div = document.createElement("div");
+        let img = document.createElement("img");
+
+        img.src = `../images/${i.toString()}.jpg`;
+        div.appendChild(img);
+        div.setAttribute("onclick", `selectLibraryImage(${i.toString()})`)
+
+        document.getElementById("upload-image-library").appendChild(div);
+    }
 
     refreshNodes();
 }
